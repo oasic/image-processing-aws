b20729aedbaba9c70417cd0913a4d82581df24b6
oasic <j@oasic.net>
2024-11-07 12:33:25 -0500

    Created basic functionality to validate image URL, request image, filter it, return image, and then delete image locally

--- project starter code/server.js
+++ project starter code/server.js
@@ -26,6 +26,56 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+app.get("/filteredimage", async (req, res) => {
+  // Retrieve image_url parameter and validate
+  let image_url = req.query.image_url;
+
+  if (!image_url) {
+    // if image_url is not present in the query parameters, return a 400 Bad Request
+    return res.status(400).send({ message: 'No image_url parameter provided.' });
+  }
+
+  // is image_url a valid URL?
+  let url;
+  try {
+    url = new URL(image_url);
+  } catch (_) {
+    return res.status(400).send({ message: 'Invalid image at ' + image_url + '. Please try again with a valid URL.' });
+  }
+
+  // is image file extension supported by jimp?
+  const imageExtensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.gif'];
+
+  const pathname = url.pathname.toLowerCase();
+
+  const isSupportedImage = imageExtensions.some(ext => pathname.endsWith(ext));
+
+  if (!isSupportedImage) {
+    // If the URL does not end with a known image extension, return a 400 Bad Request
+    return res.status(400).send({ message: 'Sorry, we can\'t handle that image type. :(' });
+  }
+
+  try {
+    // filter the image
+    const outPath = await filterImageFromURL(image_url);
+
+    // send file in the response
+    res.sendFile(outPath, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ message: 'Sorry, but we hand an error processing your image at ' + image_url + '. Please try again later.' });
+      } else {
+        // delete files
+        deleteLocalFiles([outPath]);
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(422).send({ message: 'Despite our best efforts, we were unable to process the image at ' + image_url });
+  }
+});
+
 
     /**************************************************************************** */
 
